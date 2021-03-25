697d1ea695a471ae5418db1003d83af9aa6c91d5
Mohamed Raza <razavistag@outlook.com>
2021-03-25 23:29:20 +0530

    init Project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+// Import  CSS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Global Use
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+import "material-design-icons-iconfont/dist/material-design-icons.css"; 
+import '@fortawesome/fontawesome-free/css/all.css'  
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'fa',
+    iconfont: "md",
+
+  },
+});
