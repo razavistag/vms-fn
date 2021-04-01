e03c89924882549ea69a06ee6565cedc7f748507
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2021-04-01 15:59:14 +0530

    screen request for full and exit are integrated frontend logout implemented

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -6,16 +6,19 @@
       </v-btn> -->
       Dashboard
       <v-spacer></v-spacer>
-       <v-btn small icon>
+      <v-btn small icon @click="getFullScreen" v-if="!fullscreen">
         <v-icon small>mdi-arrow-expand-all</v-icon>
       </v-btn>
-      <v-btn small icon>
+      <v-btn small icon color="white" dark @click="exitFullScreen" v-else>
+        <v-icon>mdi-arrow-expand</v-icon>
+      </v-btn>
+      <v-btn small icon @click="logout">
         <v-icon small>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-navigation-drawer
-    elevation
+      elevation
       app
       :color="sideBar"
       :expand-on-hover="true"
@@ -50,8 +53,8 @@ export default {
   name: "DashboardLayout",
   data() {
     return {
-      headerColor:'primary',
-      sideBar:'indigo lighten-4',
+      headerColor: "primary",
+      sideBar: "indigo lighten-4",
       items: [
         {
           title: "DASHBOARD",
@@ -74,7 +77,61 @@ export default {
           to: "",
         },
       ],
+      fullscreen: false,
     };
+  },
+  mounted() {
+    console.log(localStorage.getItem("fullScreen"));
+    if (localStorage.getItem("fullScreen") == 1) {
+      this.fullscreen = true;
+
+      console.log("full screen");
+    } else {
+      console.log("normal screen");
+      this.fullscreen = false;
+    }
+  },
+  methods: {
+    logout() {
+      console.log("loggedout");
+      let url = '/usr_logout'
+      this.$http
+        .get(url)
+        .then((response) => {
+          console.log(response.data);
+         console.log(response)
+         localStorage.removeItem('token')
+          this.$router.push({
+              name: "Login",
+            });
+        })
+        .catch((response) => {
+          console.log("Error Fround. Project Not Saved");
+        });
+    },
+    getFullScreen() {
+      document.documentElement
+        .requestFullscreen()
+        .then(() => {
+          this.fullscreen = true;
+          localStorage.setItem("fullScreen", 1);
+          console.log("fullscreen activated");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    exitFullScreen() {
+      document
+        .exitFullscreen()
+        .then(() => {
+          this.fullscreen = false;
+          localStorage.setItem("fullScreen", 0);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
   },
 };
 </script>
